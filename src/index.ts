2caae52123a57c325871cb0c009e02cb490fac0a
Zane Bauman <zanebauman55@gmail.com>
2022-01-27 17:34:01 -0500

    Start on checking all types

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ export default function deterministicHash(
 
 };
 
-export function deterministicString(input: unknown){
+export function deterministicString(input: unknown): string{
 
 	if(typeof input === 'string'){
 
@@ -29,25 +29,37 @@ export function deterministicString(input: unknown){
 		return input.toString();
 
 	}else if(
+		input === undefined ||
+		input === null ||
 		typeof input === 'bigint' ||
 		typeof input === 'boolean' ||
 		typeof input === 'number' ||
-		input === undefined ||
-		input === null ||
 		typeof input !== 'object'
 	){
 
 		//cast to string for any of these
 		return `${input}`;
 
-	}else if(Array.isArray(input)){
+	}else if(
+		input instanceof Date ||
+		input instanceof RegExp || 
+		input instanceof Error ||
+		input instanceof WeakMap || //non-iterable
+		input instanceof WeakSet    //non-iterable
+	){
 
-		let ret = '[';
+		//add the constructor as a key, use simple `toString`
+		return `(${input.constructor.name}:${input.toString()})`;
 
-		//add all of the key value pairs
-		for(let i = 0; i < input.length; i++){
+	}else if(input instanceof Set){
 
-			ret += `(${i}:${deterministicString(input[i])}),`;
+		//add the constructor as a key
+		let ret = `(${input.constructor.name}:[`;
+
+		//add all unique values
+		for(const val of input.values()){
+
+			ret += `(${deterministicString(val)}),`;
 
 		}
 
@@ -55,10 +67,74 @@ export function deterministicString(input: unknown){
 
 		return ret;
 
-	}else{
+	}else if(
+		Array.isArray(input) ||
+		input instanceof Int8Array ||
+		input instanceof Uint8Array ||
+		input instanceof Uint8ClampedArray ||
+		input instanceof Int16Array ||
+		input instanceof Uint16Array ||
+		input instanceof Int32Array ||
+		input instanceof Uint32Array ||
+		input instanceof Float32Array ||
+		input instanceof Float64Array ||
+		input instanceof BigInt64Array ||
+		input instanceof BigUint64Array
+	){
 
 		//add the constructor as a key
-		let ret: string = `(${deterministicString(input.constructor)}:[`;
+		let ret = `(${input.constructor.name}:[`;
+
+		for(const [k, v] of input.entries()){
+
+			ret += `(${k}:${deterministicString(v)}),`;
+
+		}
+
+		ret += ']';
+
+		return ret;
+
+	}else if(
+		input instanceof ArrayBuffer ||
+		input instanceof SharedArrayBuffer
+	){
+
+		if(input.byteLength % 8 === 0){
+
+			return deterministicString(new BigUint64Array(input));
+
+		}else if(input.byteLength % 4 === 0){
+
+			return deterministicString(new Uint32Array(input));
+
+		}else if(input.byteLength % 2 === 0){
+
+			return deterministicString(new Uint16Array(input));
+
+		}else{
+
+			let ret = '(';
+
+			for(let i = 0; i < input.byteLength; i++){
+
+				ret += deterministicString(new Uint8Array(input.slice(i, i+1)));
+
+			}
+
+			ret += ')';
+
+			return ret;
+
+		}
+
+	}else if(
+		input instanceof Map ||
+		isPlainObject(input)
+	){
+
+		//add the constructor as a key
+		let ret: string = `(${input.constructor.name}:[`;
 
 		//get key/value pairs
 		const entries = Object.entries(input);
@@ -77,6 +153,68 @@ export function deterministicString(input: unknown){
 
 		return ret;
 
+	}else{
+
+		//add the constructor as a key
+		let ret: string = `(${input.constructor.name}:[`;
+
+		const allEntries: [string, unknown][] = [];
+
+		for(const k in input){
+
+			allEntries.push([
+				`${k}`,
+				//have to ignore because `noImplicitAny` is `true` but this is implicitly `any`
+				//@ts-ignore
+				input[k]
+			]);
+
+		}
+
+		//sort alphabetically by keys
+		allEntries.sort(([a],[b])=>a.localeCompare(b));
+
+		//add all of the key/value pairs
+		for(const [k, v] of allEntries){
+
+			ret += `(${k}:${deterministicString(v)}),`;
+
+		}
+
+		ret += '])';
+
+		return ret;
+
 	}
 
 }
+
+const objConstructorString = Function.prototype.toString.call(Object);
+export function isPlainObject(value: unknown){
+
+	//base object
+	if(
+		typeof value !== 'object' ||
+		value === null ||
+		Object.prototype.toString.call(value) !== '[object Object]'
+	){
+		return false;
+	}
+
+	//get the prototype
+	const proto = Object.getPrototypeOf(value);
+
+	//no prototype === all good
+	if(proto === null){
+		return true;
+	}
+
+	// validate that the constructor is `Object`
+	const cTor = (Object.prototype.hasOwnProperty.call(proto, 'constructor') && proto.constructor);
+	return (
+		typeof cTor === 'function' &&
+		cTor instanceof cTor &&
+		Function.prototype.toString.call(cTor) === objConstructorString
+	);
+
+};
